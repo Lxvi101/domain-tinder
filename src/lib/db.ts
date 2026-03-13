53354529219902b557e3b1a3b0000ecd485ba28b
Levi B <ivelnator@gmail.com>
2026-03-13 17:11:11 +0100

    pgdb

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,9 +1,4 @@
-import Database from "better-sqlite3";
-import { drizzle } from "drizzle-orm/better-sqlite3";
-import path from "path";
+import { drizzle } from "drizzle-orm/node-postgres";
 import * as schema from "./schema";
 
-const sqlite = new Database(path.join(process.cwd(), "sqlite.db"));
-sqlite.pragma("journal_mode = WAL");
-
-export const db = drizzle(sqlite, { schema });
+export const db = drizzle(process.env.DATABASE_URL!, { schema });
